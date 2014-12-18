e01b20d2960f5d81f30067a7bd69b685b6860a63
Simon Johansson <simon@simonjohansson.com>
2014-12-18 16:00:58 +0100

    Dont make syslog connection a requirement.

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var (
 	debug            = kingpin.Flag("debug", "Enable debug mode. This disables forwarding to syslog").Bool()
 	uaaEndpoint      = kingpin.Flag("uaa-endpoint", "UAA endpoint.").Required().String()
 	dopplerEndpoint  = kingpin.Flag("doppler-endpoint", "UAA endpoint.").Required().String()
-	syslogServer     = kingpin.Flag("syslog-server", "Syslog server.").Required().String()
+	syslogServer     = kingpin.Flag("syslog-server", "Syslog server.").String()
 	subscriptionId   = kingpin.Flag("subscription-id", "Id for the subscription.").Default("firehose").String()
 	firehoseUser     = kingpin.Flag("firehose-user", "User with firehose permissions.").Default("doppler").String()
 	firehosePassword = kingpin.Flag("firehose-password", "Password for firehose user.").Default("doppler").String()
