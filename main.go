94aa07864269bd054f063433c8f0567f5d520c28
Simon Johansson <simon@simonjohansson.com>
2015-03-19 18:04:14 +0100

    Update version

--- main.go
+++ main.go
@@ -86,7 +86,7 @@ func setupLogging(syslogServer string, debug bool) {
 }
 
 func main() {
-	kingpin.Version("0.0.1")
+	kingpin.Version("0.0.1 - 2a962c4")
 	kingpin.Parse()
 
 	setupLogging(*syslogServer, *debug)
