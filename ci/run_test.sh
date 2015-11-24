9d7ee5e35578421336c95e4fbd441d3561f81fc8
Etourneau Gwenn <gwenn.etourneau@gmail.com>
2015-11-24 18:34:05 +0900

    Adding To the path

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd firehose-to-syslog-source/
+cp -aR . /go/src/github.com/cloudfoundry-community/firehose-to-syslog/
+cd /go/src/github.com/cloudfoundry-community/firehose-to-syslog/
 go get github.com/tools/godep
 godep restore
 go get github.com/onsi/ginkgo/ginkgo
