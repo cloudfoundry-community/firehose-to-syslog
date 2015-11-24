dc012b52529e613337ec83b99134ed32a8cb740a
Etourneau Gwenn <gwenn.etourneau@gmail.com>
2015-11-24 18:35:41 +0900

    Create dir

--- ci/run_test.sh
+++ ci/run_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd firehose-to-syslog-source/
+mkdir -p /go/src/github.com/cloudfoundry-community/firehose-to-syslog/
 cp -aR . /go/src/github.com/cloudfoundry-community/firehose-to-syslog/
 cd /go/src/github.com/cloudfoundry-community/firehose-to-syslog/
 go get github.com/tools/godep
