f97b15963393cc618a5e9687834eda902e9cec93
Anwar Chirakkattil <achirakkattil@pivotal.io>
2016-06-10 17:00:09 -0500

    Made log formatter a configurable option.

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ var (
 	extraFields        = kingpin.Flag("extra-fields", "Extra fields you want to annotate your events with, example: '--extra-fields=env:dev,something:other ").Default("").OverrideDefaultFromEnvar("EXTRA_FIELDS").String()
 	modeProf           = kingpin.Flag("mode-prof", "Enable profiling mode, one of [cpu, mem, block]").Default("").OverrideDefaultFromEnvar("MODE_PROF").String()
 	pathProf           = kingpin.Flag("path-prof", "Set the Path to write profiling file").Default("").OverrideDefaultFromEnvar("PATH_PROF").String()
+	logFormatterType   = kingpin.Flag("log-formatter-type", "Log formatter type to use. Valid options are text, json. If none provided, defaults to json.").Envar("LOG_FORMATTER_TYPE").String()
 )
 
 const (
@@ -45,7 +46,7 @@ func main() {
 	kingpin.Parse()
 	logging.LogStd(fmt.Sprintf("Starting firehose-to-syslog %s ", version), true)
 
-	logging.SetupLogging(*syslogServer, *debug)
+	logging.SetupLogging(*syslogServer, *debug, *logFormatterType)
 
 	c := cfclient.Config{
 		ApiAddress:        *apiEndpoint,
@@ -86,7 +87,7 @@ func main() {
 		case "block":
 			defer profile.Start(profile.BlockProfile, profile.ProfilePath(*pathProf)).Stop()
 		default:
-			// do nothing
+		// do nothing
 		}
 	}
 
