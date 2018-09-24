a6798c66f8518f93a0b150846b2b60e91144eed6
Pierre Delagrave <pdelagrave@pivotal.io>
2018-09-24 09:59:02 -0400

    Worked with pivotal-hargun-suri to add a new 'json-cee' log formatter type, allowing a rsyslog server to parse the JSON/CEE in the message.

--- logging/logging_suite_test.go
+++ logging/logging_suite_test.go
@@ -1,6 +1,11 @@
 package logging
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/stvp/go-udp-testing"
+	"math/rand"
+	"strings"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -11,6 +16,28 @@ import (
 func TestEvents(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Logging Suite")
+}
+
+// Can't use ginkgo because go-udp-testing requires to be passed testing.T
+func TestJSONCeeLoggingFormatter(t *testing.T) {
+	RegisterTestingT(t)
+
+	port := rand.Intn(65535 - 1080) + 1081
+	listeningUDPSocket := fmt.Sprintf("localhost:%d", port)
+
+	udp.SetAddr(listeningUDPSocket)
+	result := udp.ReceiveString(t, func() {
+		logging := NewLogging(listeningUDPSocket, "udp", "json-cee", "", false, true)
+		logging.Connect()
+		logging.ShipEvents(nil, "msg field content")
+	})
+
+	Ω(result).Should(ContainSubstring("@cee:"))
+
+	jsonPayload := strings.Split(result, "@cee:")[1]
+	var payload map[string]interface{}
+	json.Unmarshal([]byte(jsonPayload), &payload)
+	Ω(payload["msg"]).Should(Equal("msg field content"))
 }
 
 var _ = Describe("Logging", func() {
