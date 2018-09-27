ca3bd373f6ee5f60e27f78812b3324f039f349d9
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2018-09-27 16:48:01 +1000

    Return error if no entity data returned for record

--- caching/caching_lazyfill.go
+++ caching/caching_lazyfill.go
@@ -177,6 +177,9 @@ func (c *CacheLazyFill) fetchEntityListFromAPI(entityType string) (map[string]*e
 		}
 
 		for _, r := range md.Resources {
+			if r.Entity == nil {
+				return nil, fmt.Errorf("nil entity returned, URL: %s, GUID of nil resource: %s", url, r.Metadata.GUID)
+			}
 			rv[r.Metadata.GUID] = r.Entity
 		}
 
